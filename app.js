7079ee646276b4388c123aa0b898faea0fb5fd6b
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-07-30 13:14:39 -0400

    button functionality js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 (function iife(d, w) {
+  const menu = d.querySelector('#mobile-menu');
+  const menuLink = d.querySelector('.navbar__menu');
 
+  menu.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+  })
 
 })(document, window);
