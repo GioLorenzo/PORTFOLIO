238a16350ca87adbbf575a6410f21ee1473dbac8
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-08-01 10:36:52 -0400

    mobile menu closing functionality when links clicked

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 (function iife(d, w) {
   const menu = d.querySelector('#mobile-menu');
   const menuLink = d.querySelector('.navbar__menu');
+  const mobileWorksLink = d.querySelector('.work__nav__link');
+  const homeLink = d.querySelector('.home__link');
 
-  menu.addEventListener('click', () => {
+  let workArray = [menu, mobileWorksLink, homeLink];
+
+  workArray.forEach(x => {x.addEventListener('click', () => {
     menu.classList.toggle('is-active');
     menuLink.classList.toggle('active');
+  })
   });
 
 })(document, window);
