4fc2e01b57a929e5a43c9bd426147fdadd873e9d
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-08-09 14:31:23 -0400

    animations for projects

--- app.js
+++ app.js
@@ -6,22 +6,80 @@
 
   let workArray = [menu, mobileWorksLink, homeLink];
 
-  workArray.forEach(x => {x.addEventListener('click', () => {
-    menu.classList.toggle('is-active');
-    menuLink.classList.toggle('active');
-  })
+  workArray.forEach(x => {
+    x.addEventListener('click', () => {
+      menu.classList.toggle('is-active');
+      menuLink.classList.toggle('active');
+    })
   });
 
-  gsap.from('.anim1', { duration: 1, y: -80 });
-  gsap.from('.anim2', { duration: 1.5, opacity: 0, y: 100, });
-  gsap.from('.anim3', { duration: 2, delay: 1, y: -100, opacity: 0 });
-  gsap.from('.anim4', { duration: 1, delay: 2.1, y: 200, opacity: 0 });
+  gsap.from('.anim1', {
+    duration: 1,
+    y: -80
+  });
+  gsap.from('.anim2', {
+    duration: 1.5,
+    opacity: 0,
+    y: 100,
+  });
+  gsap.from('.anim3', {
+    duration: 2,
+    delay: 1,
+    y: -100,
+    opacity: 0
+  });
+  gsap.from('.anim4', {
+    duration: 1,
+    delay: 2.1,
+    y: 200,
+    opacity: 0
+  });
   let tl = gsap.timeline({
     scrollTrigger: {
       trigger: '.anim5',
+      trigger: 'anim6',
+      trigger: 'anim7',
+      trigger: 'anim8',
+      trigger: 'anim9',
+      trigger: 'anim10'
     }
   });
 
-  tl.from('.anim5', {duration: 1, opacity: 0, y: 40, delay: .3})
+  tl.from('.anim5', {
+    duration: 1,
+    opacity: 0,
+    y: 40,
+    delay: .3
+  })
+  tl.from('.anim6', {
+    duration: 1,
+    opacity: 0,
+    y: 40,
+    delay: .3
+  })
+  tl.from('.anim7', {
+    duration: 1,
+    opacity: 0,
+    y: 40,
+    delay: .3
+  })
+  tl.from('.anim8', {
+    duration: 1,
+    opacity: 0,
+    y: 40,
+    delay: .3
+  })
+  tl.from('.anim9', {
+    duration: 1,
+    opacity: 0,
+    y: 40,
+    delay: .3
+  })
+  tl.from('.anim10', {
+    duration: 1,
+    opacity: 0,
+    y: 40,
+    delay: .3
+  })
 
 })(document, window);
