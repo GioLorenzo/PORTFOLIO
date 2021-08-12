d01835ed4701df6a0aeddbbc187c274aff89d8d7
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-08-12 19:19:58 -0400

    .

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 (function iife(d, w) {
-  const menu = d.querySelector('#mobile-menu');
-  const menuLink = d.querySelector('.navbar__menu');
-  const mobileWorksLink = d.querySelector('.work__nav__link');
-  const homeLink = d.querySelector('.home__link');
-
-  let workArray = [menu, mobileWorksLink, homeLink];
-
-  workArray.forEach(x => {
-    x.addEventListener('click', () => {
-      menu.classList.toggle('is-active');
-      menuLink.classList.toggle('active');
-    })
-  });
-
-  gsap.from('.anim1', {
+   gsap.from('.anim1', {
     duration: 1,
     y: -80
   });
@@ -98,5 +84,20 @@
       trigger: '.anim10',
     }
   })
+
+
+  const menu = d.querySelector('#mobile-menu');
+  const menuLink = d.querySelector('.navbar__menu');
+  const mobileWorksLink = d.querySelector('.work__nav__link');
+  const homeLink = d.querySelector('.home__link');
+
+  let workArray = [menu, mobileWorksLink, homeLink];
+
+  workArray.forEach(x => {
+    x.addEventListener('click', () => {
+      menu.classList.toggle('is-active');
+      menuLink.classList.toggle('active');
+    });
+  });
 
 })(document, window);
